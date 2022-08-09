6749e642269b05fb32d669616042431b3057b30a
Walid Sahnoun / وليد سحنون <77199554+WalidSahnoun0001@users.noreply.github.com>
2022-08-09 09:23:08 +0100

    prodcut landing page

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <header>
+      <a href="https://jvz3.com/c/2464369/194909/" target="_blank"><img src="./assets/Banner728-1.jpg" alt=""></a>
+      <h2>Keywords Research Tool</h2>
+      <p>
+        If you are looking for a keywords research tool, you are in the right place.
+        I'm promoting a keywords research tool called <a href="#links">Keysearch</a>
+      </p>
+    </header>
+      <div class="line"></div>
+    <div class="product">
+        <h2 class="title">Keysearch</h2>
+        <div class="product-infos">
+          <div class="definition">
+            - Keysearch is not just a keyword research tool it's a full-fledged SEO powerhouse. Giving you all the tools you need to spy on your competitors and track your SEO efforts.
+          </div>
+          <div class="features">
+            <span>Web Based</span>
+            <span>Keyword Suggestions</span>
+            <span>In-Depth Keyword Research</span>
+            <span>Keyword Difficulty Checker</span>
+            <span>SEO Competition Analysis</span>
+            <span>Rank Tracking</span>
+            <span>Brainstorm Niche Ideas</span>
+            <span>YouTube Research</span>
+            <span>Backlink Checker</span>
+            <span>Webpage Audit Analyzer</span>
+            <span>Competitive Analysis</span>
+            <span>Content Assistant</span>
+          </div>
+        </div>
+    </div>
+    <div class="line"></div>
+    <div class="discount">
+      You this code to get 20% off [Only for Starter and Pro]: <span>KSDISC</span>
+    </div>
+    <div class="links" id="links">
+      <a href="https://jvz4.com/c/2464369/194909/" target="_blank">Keysearch Starter 17$</a>
+      <a href="https://jvz4.com/c/2464369/195039/" target="_blank">Keysearch Pro 34$</a>
+      <a href="https://jvz1.com/c/2464369/236407/" target="_blank">Keysearch Starter Annual 169$</a>
+      <a href="https://jvz4.com/c/2464369/236409/" target="_blank">Keysearch Pro Annual 279$</a>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .line {
+    position: relative;
+    width: 100%;
+    height: 1px;
+    background-color: rgba(102, 102, 102, 0.411);
+    margin: 50px 0;
+  }
+  section {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 0); 
+  }
+  section header {
+    margin: 20px 0;
+    color: #333;
+  }
+  section header img {
+    width: 100%;
+  }
+  section header h2 {
+    font-size: 25px;
+    margin: 10px 0;
+    text-align: center;
+  }
+  section header p {
+    line-height: 1.8;
+  }
+  section header p a {
+    color: #54BAB9;
+    font-weight: bold;
+    font-size: 18px;
+  } 
+  section .product {
+    color: #333;
+  }
+  section .product .title {
+    margin-bottom: 10px;
+  }
+  section .product .product-infos .definition {
+    margin: 20px 0;
+    line-height: 1.8;
+  }
+  section .product .product-infos .features span {
+    display: block;
+    position: relative;
+    margin: 10px 20px;
+  }
+  section .product .product-infos .features span::before {
+    position: absolute;
+    content: '';
+    width: 10px;
+    height: 10px;
+    background-color: #54BAB9;
+    left: -20px;
+    top: 7px;
+    border-radius: 5px;
+  }
+  section .discount span {
+    color: #54BAB9;
+    font-size: 20px;
+  }
+  section .links a {
+    display: block;
+    margin: 20px 0;
+    color: #54BAB9;
+    font-size: 18px;
+  }
+</style>
